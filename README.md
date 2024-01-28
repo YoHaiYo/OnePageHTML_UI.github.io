e75bc4e67a733c4cdd57277f585b29dfb9893e92
SSH <12si47bun@gmail.com>
2024-01-28 17:16:37 +0900

    경로데이터 데이터로 관리. 푸터. 폰트 수정

--- README.md
+++ README.md
@@ -1,12 +1,8 @@
 # 소개
-html 한페이지로 구성되어 복붙만하면 실행될 수 있게 작성된 초보자용 UI 컴포넌트 모음입니다. 
+html 한 페이지로 구성되어 복붙만하면 실행될 수 있게 작성된 초보자용 UI 모음입니다. 
 
-# 책갈피 (index)
-https://yohaiyo.github.io/UI-component-for-beginner.github.io/
+# 책갈피 사이트 바로가기
+https://yohaiyo.github.io/OnePageHTML_UI.github.io/
 
-# 사용법
-1. 필요한 기능 파일 찾기. 
-2. 그냥 전체복사 
-3. 실행해서 확인.
 
-**반응형 작업용 추천 플러그인** : https://chromewebstore.google.com/detail/responsive-viewer/inmopeiepgfljkpkidclfgbgbmfcennb?hl=ko
+
---
+++ data/pathdata.js
@@ -0,0 +1,198 @@
+const filepath = [
+  {
+    foldernm: "basic",
+    fileinfo: [
+      {
+        filenm: "coloring-data-attribute",
+        filetitle: "html데이터 속성활용",
+        starnum: "0"
+      },
+      {
+        filenm: "dropdown-family",
+        filetitle: "패밀리사이트 드롭박스 구현",
+        starnum: "0"
+      },
+      {
+        filenm: "filtering-data-attribute",
+        filetitle: "데이터속성으로 초간단 필터링",
+        starnum: "1"
+      },
+      {
+        filenm: "filtering-data-manage-outerdata",
+        filetitle: "데이터속성을 활용한 외부데이터로 태그생성",
+        starnum: "0"
+      },
+      {
+        filenm: "header-calc-height",
+        filetitle: "height()로 헤더높이 자동, 스크롤시 헤더 접기",
+        starnum: "0"
+      },
+      {
+        filenm: "header_hover_all_menu",
+        filetitle: "헤더-호버시 소메뉴 전체나오기",
+        starnum: "0"
+      },
+      {
+        filenm: "header_hover_one_menu",
+        filetitle: "헤더-호버시 소메뉴 하나 나오기",
+        starnum: "0"
+      },
+      {
+        filenm: "header_scroll_selected_colorchange",
+        filetitle: "헤더스크롤,위로버튼,선택메뉴 색바꾸기",
+        starnum: "0"
+      },
+      {
+        filenm: "hoverhashcard",
+        filetitle: "호버됬을때 해쉬태그나오면서 이펙트주기",
+        starnum: "0"
+      },
+      {
+        filenm: "language-select-menu",
+        filetitle: "언어선택 배열연습",
+        starnum: "0"
+      },
+      {
+        filenm: "modal",
+        filetitle: "Modal 기능",
+        starnum: "0"
+      },
+      {
+        filenm: "popup-oneday-no-look",
+        filetitle: "팝업-하루동안-보지않기",
+        starnum: "1"
+      },
+      {
+        filenm: "popup-slide",
+        filetitle: "팝업슬라이드",
+        starnum: "0"
+      },
+      {
+        filenm: "quick-menu",
+        filetitle: "퀵메뉴",
+        starnum: "0"
+      },
+      {
+        filenm: "reple",
+        filetitle: "댓글달기",
+        starnum: "0"
+      },
+      {
+        filenm: "spyscroll",
+        filetitle: "Bootstrap 스크롤스파이",
+        starnum: "0"
+      },
+      
+    ]
+  },
+  {
+    foldernm: "Silde-swiper",
+    fileinfo: [
+      {
+        filenm: "bullet-custom",
+        filetitle: "스와이퍼 블렛 커스텀",
+        starnum: "2"
+      },
+      {
+        filenm: "three_type",
+        filetitle: "SWIPER 커스텀 3종 세트",
+        starnum: "1"
+      },
+      {
+        filenm: "bgcolor-chage-slide",
+        filetitle: "슬라이드변경시 색변경",
+        starnum: "0"
+      },
+      {
+        filenm: "double-slide",
+        filetitle: "원버튼 이중 슬라이드",
+        starnum: "0"
+      },
+      {
+        filenm: "opposite-direction-slide",
+        filetitle: "각각 양쪽방향으로 슬라이드",
+        starnum: "0"
+      },
+      {
+        filenm: "slide-css-custom",
+        filetitle: "스와이퍼 CSS 커스텀",
+        starnum: "0"
+      },
+      {
+        filenm: "slide-cubic",
+        filetitle: "주사위 스와이퍼",
+        starnum: "0"
+      },
+      {
+        filenm: "slide-plugin-customazing",
+        filetitle: "스와이퍼 변수값만 바꿔서 커스텀마이징",
+        starnum: "0"
+      },
+      {
+        filenm: "slide-plugin",
+        filetitle: "스와이퍼 변수값만 바꿔서 커스텀마이징2",
+        starnum: "0"
+      },
+      {
+        filenm: "slide-side-two-half",
+        filetitle: "양쪽에 반개씩 보이는 슬라이드",
+        starnum: "0"
+      },
+      {
+        filenm: "stop-play-slide",
+        filetitle: "stop/play버튼있는 슬라이드",
+        starnum: "0"
+      },
+    ]
+  },
+  {
+    foldernm: "Slide-vanilla",
+    fileinfo: [
+      {
+        filenm: "slide-jquery-fadein",
+        filetitle: "슬라이드집중-페이드인",
+        starnum: "0"
+      },
+      {
+        filenm: "slide-jquery-horizontal",
+        filetitle: "수평 자동 슬라이드",
+        starnum: "0"
+      },
+      {
+        filenm: "slide-jquery-horizontal2",
+        filetitle: "수평 자동 슬라이드2",
+        starnum: "0"
+      },
+      {
+        filenm: "slide-jquery-vertical",
+        filetitle: "수직 자동 슬라이드",
+        starnum: "0"
+      },
+      {
+        filenm: "slide-jquery-vertical-2",
+        filetitle: "수직 자동 슬라이드2",
+        starnum: "0"
+      },
+      {
+        filenm: "tricolor-slide",
+        filetitle: "삼색국기 슬라이드",
+        starnum: "1"
+      },
+    ]
+  },  
+  {
+    foldernm: "navbar",
+    fileinfo: [      
+      {
+        filenm: "basic",
+        filetitle: "기본 네비바",
+        starnum: "1"
+      },
+      {
+        filenm: "navbar-2depth",
+        filetitle: "부트5-네비바-2depth",
+        starnum: "0"
+      },
+    ]
+  }
+]
